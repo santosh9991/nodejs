40f26c4c1845eb313b712e9b318bb617d7e59aec
santosh9991 <santu1santosh@gmail.com>
2018-02-18 08:15:24 -0800

    AddNote function modified

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ const yargs = require('yargs')
 const notes = require('./notes')
 // Yargs is a utility to access command line arguments efficiently
 const argv = yargs.argv
-console.log('Process',process.argv)
+// console.log('Process',process.argv)
+/*command line arguments are available on process object which is globle
+Process [ '/usr/bin/node',
+  '/home/coder/Desktop/personal_workspace/node_notes/app.js',
+  'read',
+  'write',
+  '--title=secret' ]
+*/
 console.log('Yarg',argv)
+/*
+Yarg { _: [ 'read', 'write' ], title: 'secret', '$0': 'app.js' }
+*/
+
 // Accessing the command line arguments through terminal
-var command = process.argv[2];
+var command = argv._[0];// can also do process.argv[2]
 
 if (command === 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title,argv.body)
 }else if (command === 'list') {
-   console.log('Listing all notes');
+  notes.getAll();
 } else if (command === 'read') {
-   console.log('Reading notes');
+  notes.getNote(argv.title);
 } else if (command === 'write') {
-   console.log('write notes');
-} else {
+  notes.writeNote(argv.title,argv.body);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title,argv.body);
+}
+else {
   console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,11 +1,38 @@
 console.log("Starting notes.js")
+const fs = require('fs');
 
 // console.log(module);
-module.exports.addNote = () =>{
-  console.log('addNote');
-  return 'New node';
+var addNote = (title, body) => {
+  notes = []
+  note = {
+    title,
+    body
+  };
+  notes.push(notes)
+fs.writeFileSync('notes-data.js',JSON.stringify(notes))
+  console.log('Adding note', title, body);
+};
+var getAll = () => {
+  console.log('Getting all notes');
+};
+var getNote = (title) => {
+  console.log('Reading note',title);
+};
+var writeNote = (title,body) => {
+  console.log('Writing note',title,body);
+};
+var removeNote = () => {
+  console.log('Writing note');
+};
+module.exports = {
+  addNote,//shortcut for addNote:addNote beacuse in ES6 both are same
+  getAll,
+  getNote,
+  writeNote,
+  removeNote
 };
 /*module is an object attached to globle name space.
 If you want any function or class to be accessable in
 other module or .js file use exports and bind it.
+For example addNote is available in app.js
 */
