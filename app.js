ccbc5ce378e8c457990cde3916afe956898daa2a
santosh9991 <santu1santosh@gmail.com>
2018-02-18 16:13:31 -0800

    Notes app and basics of node js coverd.

--- app.js
+++ app.js
@@ -5,34 +5,64 @@ const _ = require('lodash');
 const yargs = require('yargs')
 
 const notes = require('./notes')
+var titleOptions = {
+  describe:'Title of note',
+  alias:'t',
+  demand:true
+}
+
+var bodyOptions = {
+  describe:'Enter Note body',
+  demand:true,
+  alias:'b'
+}
 // Yargs is a utility to access command line arguments efficiently
-const argv = yargs.argv
-// console.log('Process',process.argv)
-/*command line arguments are available on process object which is globle
-Process [ '/usr/bin/node',
-  '/home/coder/Desktop/personal_workspace/node_notes/app.js',
-  'read',
-  'write',
-  '--title=secret' ]
-*/
+const argv = yargs
+  .command('add','Add new Note',{
+    title:titleOptions,
+    body:bodyOptions
+  })
+  .command('remove','Remove note',{
+    title:titleOptions,
+  })
+  .command('read','read note',{
+    title:titleOptions,
+  })
+  .command('list','List all notes',{
+  })
+  .help()
+  .argv
+
 console.log('Yarg',argv)
-/*
-Yarg { _: [ 'read', 'write' ], title: 'secret', '$0': 'app.js' }
-*/
+
 
 // Accessing the command line arguments through terminal
 var command = argv._[0];// can also do process.argv[2]
 
 if (command === 'add') {
-  notes.addNote(argv.title,argv.body)
+  note = notes.addNote(argv.title,argv.body)
+  if (note){
+    notes.logNote(note)
+  }
+  else{
+    console.log("Duplicate node")
+  }
 }else if (command === 'list') {
-  notes.getAll();
+  console.log(notes.getAll());
 } else if (command === 'read') {
-  notes.getNote(argv.title);
+  note = notes.getNote(argv.title)
+  if(note){
+    notes.logNote(note)
+  }
+  else {
+    console.log('No Note to read ')
+  }
 } else if (command === 'write') {
   notes.writeNote(argv.title,argv.body);
 } else if (command === 'remove') {
-  notes.removeNote(argv.title,argv.body);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note remoed ' : 'Note not removed';
+  console.log(message)
 }
 else {
   console.log('Command not recognized');
--- notes.js
+++ notes.js
@@ -1,35 +1,69 @@
 console.log("Starting notes.js")
 const fs = require('fs');
-
+var fetchNotes = () => {
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    return JSON.parse(noteString);
+  }catch (e){
+    return [];
+  }
+}
+var saveNotes = (notes) =>{
+  fs.writeFileSync('notes-data.json',JSON.stringify(notes))
+}
 // console.log(module);
 var addNote = (title, body) => {
-  notes = []
-  note = {
+  var notes = fetchNotes();
+  var note = {
     title,
     body
   };
-  notes.push(notes)
-fs.writeFileSync('notes-data.js',JSON.stringify(notes))
-  console.log('Adding note', title, body);
+
+// var duplicateValues = notes
+
+var duplicateValues = notes.filter( (note)=>note.title===title)
+if (duplicateValues.length===0){
+  notes.push(note)
+  saveNotes(notes)
+  return note;
+}
+
+  // console.log('Adding note', title, body);
 };
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 var getNote = (title) => {
+  var notes = fetchNotes();
+  var note = notes.filter((note)=>note.title === title);
+  console.log('Note::::::',note);
+  return note[0]
   console.log('Reading note',title);
 };
 var writeNote = (title,body) => {
   console.log('Writing note',title,body);
 };
-var removeNote = () => {
+var removeNote = (title) => {
+  var notes = fetchNotes()
+  var updatedNotes =notes.filter((note) =>note.title !== title)
+  saveNotes(updatedNotes)
+  return notes.length !== updatedNotes.length
   console.log('Writing note');
 };
+
+var logNote = (note)=>{
+  console.log('Note Found');
+  console.log('--');
+  console.log(`note title: ${note.title}``);
+  console.log(`note body: ${note.body}`);
+}
 module.exports = {
   addNote,//shortcut for addNote:addNote beacuse in ES6 both are same
   getAll,
   getNote,
   writeNote,
-  removeNote
+  removeNote,
+  logNote
 };
 /*module is an object attached to globle name space.
 If you want any function or class to be accessable in
